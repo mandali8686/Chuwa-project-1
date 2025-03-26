51ad5fa2db852615ad21e68683da80a2af8de232
manda <manda.li8686@gmail.com>
2025-03-26 13:18:29 -0500

    frontend and backend structure

---
+++ Frontend/product-management/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './NavBar.css';
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <NavLink 
+          to="/" 
+          className="navbar-logo"
+          onClick={closeMobileMenu}
+        >
+          Management
+        </NavLink>
+
+        
+        <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+          {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
+        
+        <ul className={`nav-menu ${isMobileMenuOpen ? 'active' : ''}`}>
+          <li className="nav-item">
+            <NavLink 
+              to="/SignIn" 
+              className={({ isActive }) => 
+                `nav-links ${isActive ? 'active' : ''}`
+              }
+              onClick={closeMobileMenu}
+            >
+              Sign In
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink 
+              to="/Cart" 
+              className={({ isActive }) => 
+                `nav-links ${isActive ? 'active' : ''}`
+              }
+              onClick={closeMobileMenu}
+            >
+              Cart
+            </NavLink>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
