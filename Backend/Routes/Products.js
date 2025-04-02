782e8d862c57bff1e2f650051254365475776b8e
manda <manda.li8686@gmail.com>
2025-04-02 12:12:19 -0500

    added auth

--- Backend/Routes/Products.js
+++ Backend/Routes/Products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const auth = require('../middlewares/auth');
 
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.post('/', auth, productController.createProduct);
+router.put('/:id', auth, productController.updateProduct);
+router.delete('/:id', auth, productController.deleteProduct);
 router.get('/', productController.getAllProducts); 
 router.get('/:id', productController.getProduct); 
 
