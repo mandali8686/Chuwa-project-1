d92aaaf4b458fb80cfd6e7741063bdc0125e92c5
manda <manda.li8686@gmail.com>
2025-04-09 17:31:21 -0500

    resolve conflict

--- Frontend/product-management/src/index.js
+++ Frontend/product-management/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProductDetails from './components/ProductDetails/ProductDetails';
+import { BrowserRouter} from 'react-router-dom';
+import store from "./redux/store"
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" exact element={<App />} />
-    <Route path="/product-details" exact element={<ProductDetails />} />
-  </Routes>
-</BrowserRouter>
-);
 
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
